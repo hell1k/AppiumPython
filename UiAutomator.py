46538d52e5ba4bc1c800b17b9cb8bc30546f3063
el_erizo <hell1k@ya.ru>
2024-02-12 17:57:11 +0700

    *

---
+++ UiAutomator.py
@@ -0,0 +1,8 @@
+import uiautomator2 as u
+
+
+def test_example():
+    d = u.connect("emulator-5554")
+    # d = u.connect("49e9905f")
+    d(resourceId="com.yapmap.yapmap:id/sign_in_button").click()
+    d.xpath('//*[@resource-id="com.yapmap.yapmap:id/email_edit_text_layout"]/android.widget.FrameLayout[1]').set_text("qwe")
