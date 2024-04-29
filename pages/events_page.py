d7ef5210683f526265ee38847dddef22d6dcb2a1
VysotskiySS <idgwynbleidd@gmail.com>
2024-04-29 11:46:59 +0700

    add new event test

--- pages/events_page.py
+++ pages/events_page.py
@@ -2,7 +2,7 @@ import time
 from random import randint
 
 import allure
-
+import random
 from tests.config import *
 from pages.base_page import BasePage
 from common.menu import Menu
@@ -21,18 +21,45 @@ class EventsPage(BasePage):
     take_a_picture_btn = 'com.android.camera2:id/bottom_bar'
     take_a_picture_done_btn = '//*[@resource-id="com.android.camera2:id/done_button"]'
     create_group_btn = 'com.yapmap.yapmap:id/action_create_group'
-    private_checkbox = '//*[@resource-id="com.yapmap.yapmap:id/is_private_switch"]//*[@resource-id="com.yapmap.yapmap:id/container"]'
-    pin_chat_checkbox = '//*[@resource-id="com.yapmap.yapmap:id/pin_chat_switch"]//*[@resource-id="com.yapmap.yapmap:id/container"]'
+    private_checkbox = '//*[@text="Private"]/..'
+    private_text = '//*[@text="Private"]'
+    pin_chat_checkbox = '//*[@text="Pin chat"]/..'
     group_name_in_list = 'com.yapmap.yapmap:id/name_text_view'
     save_btn = 'com.yapmap.yapmap:id/action_save'
     back_btn = 'com.yapmap.yapmap:id/back'
     done_photo = '//*[@resource-id="com.yapmap.yapmap:id/action_done"]'
     event_name_in_list = 'com.yapmap.yapmap:id/name_text_view'
-    type_select = "com.yapmap.yapmap:id/click_view"
+    type_selection = "com.yapmap.yapmap:id/click_view"
     types = '//androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout'
+    add_photos_btn = "com.yapmap.yapmap:id/photos_field_add_photo_text_view"
+    first_photo = '//*[@resource-id="com.yapmap.yapmap:id/images_recycler_view"]/android.view.ViewGroup[2]'
+    second_photo = '//*[@resource-id="com.yapmap.yapmap:id/images_recycler_view"]/android.view.ViewGroup[3]'
+    date_start = "com.yapmap.yapmap:id/start_date_edit_text"
+    date_end = "com.yapmap.yapmap:id/end_date_edit_text"
+    time_start = "com.yapmap.yapmap:id/start_time_edit_text"
+    time_end = "com.yapmap.yapmap:id/end_time_edit_text"
+    live_event_check_box = "com.yapmap.yapmap:id/live_event_check_box"
+    online_event_check_box = "com.yapmap.yapmap:id/online_event_check_box"
+    address_select_btn = '//*[@text="Address"]/..'
+    type_address_field = "com.yapmap.yapmap:id/auto_complete_text_view"
+    address_popup = '//*[@text="Novosibirsk, Novosibirsk Oblast, Russia"]'
+    map_plus_btn = '//*[@resource-id="com.yapmap.yapmap:id/floating_action_button"]'
+    number_of_members_field = '//*[@text="Number of members (optional)"]'
+    calendar_next_btn = '//*[@resource-id="android:id/next"]'
+    calendar_ok = '//*[@resource-id="android:id/button1"]'
+    notification_checkbox = '//*[@text="Notifications"]/..'
+    show_it_to_others_checkbox = '//*[@text="Show it to others"]/..'
+    show_me_to_this_community = '//*[@text="Show me to this community"]/..'
 
     def click_back_btn(self):
         self.click(self.d(description="Back"), "кнопка Назад")
+
+    @allure.step("Выбор случайного типа")
+    def select_random_type(self):
+        self.click(self.type_selection)
+        self.wait_element(self.types)
+        self.click(self.get_random_element(self.types), "рандомный тип")
+        self.wait_a_moment()
 
     def add_new_event(self, is_private=None):
         event_name = 'Test event_' + str(randint(0, 999999999))
@@ -49,18 +76,54 @@ class EventsPage(BasePage):
         self.wait_a_second()
         self.set_text(self.description_field, text_250, 'поле Description')
 
+        self.select_random_type()
+
+        self.swipe_to_element(self.add_photos_btn)
+        self.click(self.add_photos_btn)
+        self.click(self.first_photo)
+        # self.click(self.second_photo)
+        self.click(self.done_photo)
+
+        self.click(self.date_start)
+        self.click(self.calendar_next_btn)
+        random_date = random.randrange(1, 28)
+        self.click(f'//*[@text={str(random_date)}]', f'дата {random_date}')
+        self.click(self.calendar_ok, "кнопка Ок")
+
+        self.swipe_to_element(self.date_end)
+        self.click(self.date_end)
+        self.click(self.calendar_next_btn)
+        random_date = random.randrange(1, 28)
+        self.click(f'//*[@text={str(random_date)}]', f'дата {random_date}')
+        self.click(self.calendar_ok, "кнопка Ок")
+
+        self.click(self.online_event_check_box)
 
 
+        self.swipe_to_element(self.address_select_btn)
+        self.click(self.address_select_btn)
+        self.wait_text('Choose location')
+        self.set_text(self.type_address_field, 'Novosibirsk')
+        self.click(self.address_popup)
+        self.click(self.map_plus_btn)
+        self.wait_text('New event')
 
-        self.click(self.type_select, 'Type Selection')
-        self.wait_text('Type')
-
+        self.swipe_to_element(self.number_of_members_field)
+        random_number = str(random.randrange(5, 1000))
+        self.click(self.number_of_members_field)
+        self.wait_a_moment()
+        self.set_text(self.number_of_members_field, random_number)
 
         if is_private == 'private':
-            self.swipe_to_element(self.private_checkbox)
+            self.swipe_to_element(self.private_text)
             self.click(self.private_checkbox, 'чекбокс Private')
 
-        self.click(self.create_event_btn, "кнопка Создать канал")
+        self.swipe_to_element(self.show_me_to_this_community)
+        self.click(self.notification_checkbox, 'чекбокс Notification')
+        self.click(self.show_it_to_others_checkbox, 'чекбокс Show it to others')
+        self.click(self.show_me_to_this_community, 'чекбокс Show me to this community')
+
+        self.click(self.create_event_btn, "кнопка Создать")
         self.wait_element(self.event_name_in_list)
         self.swipe_down()
         self.wait_a_second()
--- tests/test_events.py
+++ tests/test_events.py
@@ -12,7 +12,7 @@ class TestEvents:
     @allure.title("Создание нового приватного события")
     @pytest.mark.smoke
     @pytest.mark.events
-    def test_events_create_new_private(self, authorization):
+    def test_events_create_new_private(self, login):
         page = MainPage()
         page.profile.open_events()
         page.events.add_new_event('private')
